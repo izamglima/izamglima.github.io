b9c83ca74e05a89360ef4db7d653ce056408211c
izamglima <izamglima@gmail.com>
2022-09-17 11:03:40 +0200

    change deploy yml and next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
+  assetPrefix: isProd ? '/izamglima.github.com/' : '',
+  images: {
+    unoptimized: true,
+  },
   //reactStrictMode: true,
   swcMinify: true,
   webpack: (config, options) => {
