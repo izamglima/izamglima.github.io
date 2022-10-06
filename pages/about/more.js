270a9ad2b7ecbb0f17cf79ff35dcd5423adcd7c9
izamglima <izamglima@gmail.com>
2022-10-06 11:08:36 +0200

    change in more page

--- pages/about/more.js
+++ pages/about/more.js
@@ -14,10 +14,13 @@ const More = () => {
         <div className={styles.more}>
           <div className={styles.box}>
             <p>Born and raised in <Link href="https://en.wikipedia.org/wiki/Campinas"><a>Campinas</a></Link>, Brazil. 
-              It's a big city, close to São Paulo. 
-              Full of colonial heritage (with all good and bad aspects). 
+              It's a big city, close to the capital São Paulo. 
               When as a kid, the peak of hapiness was our anual trip to the beach, 
               usually on São Paulo's coast like Ubatuba, Guarujá or Santos.
+            </p>
+
+            <p>In 2011 I meet my husband because of a friend that I studied together with. 
+              Since then we are living together.
             </p>
 
             <p>The first time I traveled abroad was in 2018. Before that I always traveled 
@@ -26,24 +29,16 @@ const More = () => {
               also, the countryside is beautiful.
             </p>
           
-            <p>In 2011 I meet my husband because of a friend that I studied together with. 
-              Since then we are living together.
-            </p>
-          
-            <p>In 2019 we decided to move to the Netherlands, where he was already working 
+            <p>In 2019 we decided to move to the Netherlands, where my husband was already working 
               remotely as a software developer. The actual move happened during the lockdowns
               in 2020. Luckily we had a small support network of friends who helped during this 
               period.
             </p>
 
-            <p>We moved with our loyal friend, Simba. unfortunately he left us in november 2021. 
-              He was a mixed german shepherd that gaves us a lot of happiness, loyalty and companionship during 10 years.
-            </p>
-
-            <p>Besides work I like to listen to music and podcasts, spend time outside walking, 
-              biking, swimming. Cooking is also a thing that I like a lot, which I learned with 
+            <p>Besides work / tech stuffs I like to listen to music and podcasts, spend time outside walking, 
+              biking or swimming. Cooking is also a thing that I like a lot, which I learned with 
               my grandmother. I appreciate art and try to do some drawings and paitings.
-              Also, I like to read about psychology, human behavior, politics, literature, spirituality and mysticism. 
+              Also, I like to read about psychology, human behavior, politics, literature and spirituality. 
             </p>
           </div>
         </div>
