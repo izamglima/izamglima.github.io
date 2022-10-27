66d6e538b3da525371c254b58241eba1602b888e
izamglima <izamglima@gmail.com>
2022-10-27 10:43:09 +0200

    small improvements to wordle

--- pages/projects/wordle.js
+++ pages/projects/wordle.js
@@ -24,7 +24,12 @@ const Wordle = () => {
       <main className="main">
         <section className={styles.wordle}>
           <h1>Wordle clone</h1>
-          <p>highly inspired by the original game</p>
+          <p>highly inspired by the original game: <br/> try to guess the 5 letters word!</p>
+          <div className={styles.explanation}>
+            <p><span className={styles.green}></span>: letter position correct</p>
+            <p><span className={styles.yellow}></span>: letter present in word but in a different position</p>
+            <p><span className={styles.grey}></span>: letter not present</p>
+          </div>
           {solution && <WordleComponent solution={solution} />}
           <small className={styles.small}>made with help of the wonderful netninja / <Link href="https://www.youtube.com/c/TheNetNinja"><a>@iamshaunjp</a></Link></small>
         </section>
